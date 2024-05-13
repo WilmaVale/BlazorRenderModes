2874c4ce6a3f89663fb7c5ea1a9eb5beb0b2c19a
Wilma Valentino <wilma.valentino@aesys.tech>
2024-05-13 09:03:08 +0200

    @rendermode InteractiveServer + modifiche a Program.cs

--- None/Program.cs
+++ None/Program.cs
@@ -4,7 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Aggiunge servizi al contenitore
 // AddRazorComponents: registra i servizi richiesti per il rendering (elaborazione) lato server di Razor Components
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    //Aggiunge servizi per supportare il rendering di componenti server interattivi in un'applicazione razor components.
+    //aka: old Blazor server with web sockets
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -23,6 +26,8 @@ app.UseAntiforgery();
 // Mappa i componenti della pagina definiti nel TRootComponent
 // specificato all'assembly specificato ed
 // esegue il rendering del componente specificato da TRootComponent quando il percorso corrisponde
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    // Configura l'applicazione per supportare Microsoft.AspNetCore.Components.Web.RenderMode.InteractiveServer render mode
+    .AddInteractiveServerRenderMode();
 
 app.Run();
